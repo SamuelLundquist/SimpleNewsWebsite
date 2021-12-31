3dc6a63ca653dd911aa3f35f4d11deb236d54737
Samuel Lundquist <50561985+SamuelLundquist@users.noreply.github.com>
2021-12-30 21:07:33 -0800

    20 min tut point.

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+// Templating Engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+// Routes
+const newsRouter = require('./src/routes/news')
+
+app.use('/', newsRouter)
+
+// Listening on port 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
