c66ce99e2daea2bdbbf9f165d4ae28aef0daad52
Samuel Lundquist <50561985+SamuelLundquist@users.noreply.github.com>
2021-12-30 21:34:32 -0800

    Tutorial 31 min mark

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const newsRouter = express.Router()
+const axios = require('axios')
 
 newsRouter.get('', async(req, res) => {
-	res.render('news')
+
+	try {
+			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			res.render('news', { articles: newsAPI.data.articles })
+			console.log(newsAPI.data.articles)
+	} catch (err) {
+		if(err.response) {
+			console.log(err.response.data)
+			console.log(err.response.status)
+			console.log(err.response.headers)
+			res.render('news', { articles : null })
+		} else if (err.requiest) {
+			res.render('news', { articles : null })
+			console.log(err.requiest)
+		} else {
+			res.render('news', { articles : null })
+			console.error('Error', err.message)
+		}
+	}
+
 })
 
 module.exports = newsRouter
