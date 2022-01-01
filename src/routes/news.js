430d72ae0a388a1c97af4868a17053157b2ae968
Samuel Lundquist <50561985+SamuelLundquist@users.noreply.github.com>
2021-12-31 16:11:35 -0800

    Update news.js
    Added api key to process environment

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,9 +2,12 @@ const express = require('express')
 const newsRouter = express.Router()
 const axios = require('axios')
 
+// [IMPORTANT] Need to define your own api key if you wish to use on your own!!!
+const apikey = process.env.NEWSAPIKEY || ''
+
 newsRouter.get('', async(req, res) => {
 	try {
-			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('news', {  title: 'Popular', articles: newsAPI.data.articles })
 	} catch (err) {
 		if(err.response) {
@@ -26,7 +29,7 @@ newsRouter.get('', async(req, res) => {
 newsRouter.post('', async(req, res) => {
 	let search = req.body.search
 	try {
-			const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=${search}&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd`)
+			const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=${search}&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('newssearch', { articles: newsAPI.data.articles, search: search })
 	} catch (err) {
 		if(err.response) {
@@ -47,7 +50,7 @@ newsRouter.post('', async(req, res) => {
 
 newsRouter.get('/sports', async(req, res) => {
 	try {
-			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=sports&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=sports&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('news', { title: 'Sports', articles: newsAPI.data.articles })
 	} catch (err) {
 		if(err.response) {
@@ -68,7 +71,7 @@ newsRouter.get('/sports', async(req, res) => {
 
 newsRouter.get('/business', async(req, res) => {
 	try {
-			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('news', { title: 'Business', articles: newsAPI.data.articles })
 	} catch (err) {
 		if(err.response) {
@@ -89,7 +92,7 @@ newsRouter.get('/business', async(req, res) => {
 
 newsRouter.get('/entertainment', async(req, res) => {
 	try {
-			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=entertainment&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=entertainment&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('news', { title: 'Entertainment', articles: newsAPI.data.articles })
 	} catch (err) {
 		if(err.response) {
@@ -110,7 +113,7 @@ newsRouter.get('/entertainment', async(req, res) => {
 
 newsRouter.get('/health', async(req, res) => {
 	try {
-			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=health&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=health&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('news', { title: 'Health', articles: newsAPI.data.articles })
 	} catch (err) {
 		if(err.response) {
@@ -131,7 +134,7 @@ newsRouter.get('/health', async(req, res) => {
 
 newsRouter.get('/science', async(req, res) => {
 	try {
-			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=science&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=science&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('news', { title: 'Science', articles: newsAPI.data.articles })
 	} catch (err) {
 		if(err.response) {
@@ -152,7 +155,7 @@ newsRouter.get('/science', async(req, res) => {
 
 newsRouter.get('/technology', async(req, res) => {
 	try {
-			const newsAPI = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=technology&language=en&sortBy=popularity&apiKey=1f31f9ff71e74fe48d07266d58b211bd')
+			const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=technology&language=en&sortBy=popularity&apiKey=${apikey}`)
 			res.render('news', { title: 'Technology', articles: newsAPI.data.articles })
 	} catch (err) {
 		if(err.response) {
